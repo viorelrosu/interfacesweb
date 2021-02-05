d3c339538ea2d2a3a7dcb9fa87c5ada464943199
Viorel Rosu <viorel.rosu@gmail.com>
2021-02-05 18:16:48 +0100

    040221

--- t6/ejercicios/ej07/assets/script.js
+++ t6/ejercicios/ej07/assets/script.js
@@ -1,30 +1,13 @@
 $(document).ready(function(){
-	/*$("ul#menu1 li.hasSubMenu").hover(
-		function(){
-			$("ul#menu1 li.hasSubMenu ul").slideUp();
-			$(this).children('ul').slideDown();
-		},
-		function(){
-		}
-	);
 
-	$("ul#menu1 li.hasSubMenu ul li").hover(
-		function(){
-			$(this).children('ul').slideDown();
-		},
-		function(){
-			//$(this).children('ul').slideUp();
-		}
-	);
-
-	$("ul#menu1 li.item").mouseenter(function(){
-		$("ul#menu1 li.hasSubMenu").children('ul').hide();
-	});*/
-
-	$('ul#menu1>li').hover(function(){
-	    	$(this).find('>ul').slideDown()
+	$('ul#menu1 li').hover(function(){
+			$(this).children('ul').stop();
+	    	$(this).children('ul').slideDown()
 		},
 	    function(){
-	    	//$(this).find('>ul').fadeOut();
-	});	
+	    	$(this).children('ul').stop();
+	    	$(this).children('ul').fadeOut();
+		}
+	);
+
 });
