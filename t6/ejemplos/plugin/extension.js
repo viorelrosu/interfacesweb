94a3f879d789c6d37dacae8cad0b3df93f230c33
Viorel Rosu <viorel.rosu@gmail.com>
2021-02-09 20:52:23 +0100

    090221 - ej08 y ej extension

---
+++ t6/ejemplos/plugin/extension.js
@@ -0,0 +1,6 @@
+(function($){
+	$.fn.contar = function() {
+		var n = this.length;
+		console.log(n);
+	};
+})(jQuery);
--- t6/ejercicios/ej08/assets/script.js
+++ t6/ejercicios/ej08/assets/script.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
 
-	
+	$(".zoom").mousemove(function(e){
+		zoom(e);
+	});
 
+	function zoom(e){
+		var x, y;
+		var zoomer = e.currentTarget;
+
+		var offsetX = e.offsetX;
+		var offsetY = e.offsetY;
+
+		x = offsetX/zoomer.offsetWidth*100;
+		y = offsetY/zoomer.offsetHeight*100;
+
+		zoomer.style.backgroundPosition = x+'% '+y+'%';
+		//console.log(zoomer.style.backgroundPosition);
+	}
 });
